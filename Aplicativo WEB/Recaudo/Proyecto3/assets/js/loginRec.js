540a1d9cff0425b3a4e0e0030e946431a5dfa573
cpierotti <33992334+cpierotti@users.noreply.github.com>
2019-08-26 09:32:49 -0500

    Continua Recaudo

---
+++ Aplicativo WEB/Recaudo/Proyecto3/assets/js/loginRec.js
@@ -0,0 +1,26 @@
+function iniciar() {
+	var username = window.localStorage.getItem('username');
+	var password = window.localStorage.getItem('password');
+
+	if(username != null && password !=null) {
+		document.getElementById("username").value = username;
+		document.getElementById("password").value = password;
+		document.getElementById("guardar").checked = true;
+	}
+}
+
+
+function cambiar(){
+
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	var check = document.getElementById("guardar").checked;
+
+	if(check == true) {
+		window.localStorage.setItem('username',username);
+		window.localStorage.setItem('password',password);
+	} else {
+		window.localStorage.removeItem('username');
+		window.localStorage.removeItem('password');
+	}
+}
